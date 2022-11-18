38750908da5a01e649ce33b0975544e931190173
Keandre Fuel <Silentfire494@gmail.com>
2022-11-18 09:35:59 -0600

    Delted 9 apps

--- Questions/question1.py
+++
@@ -1,7 +0,0 @@
-# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-# between 2000 and 3200 (both included).The numbers obtained should be printed in a comma-separated sequence on a single line.
-n = []
-
-for n in range(2000, 3201):  # range function is used to generate a sequence of numbers
-    if (n % 7 == 0) and (n % 5 != 0):  # if statement is used to check the condition
-        print(n, end=",")  # print function is used to print the output
--- Questions/question4.py
+++
@@ -1,6 +0,0 @@
-# Write a program which accepts a sequence of comma-separated numbers from console
-# and generate a list and a tuple which contains every number.
-n = input("Enter a sequence of comma-separated numbers: ")
-n = n.split(",")  # split function is used to split the string into a list
-print(n)
-print(tuple(n))  # tuple function is used to convert the list into a tuple
